aa4c0cb4b361a25ab0921a235a7d9d7f3ef82ecd
Ndao-FullStack <ndaoibrahima037@gmail.com>
2023-08-25 22:58:32 +0000

    My first project on ruby OOP

--- lib/event.rb
+++ lib/event.rb
@@ -1,3 +1,57 @@
-class Event
+require 'time'
 
+class Event
+  attr_accessor :start_date, :duration, :title, :attendees
+
+  def initialize(start_date, duration, title, attendees)
+    @start_date = Time.parse(start_date)
+    @duration = duration
+    @title = title
+    @attendees = attendees
+  end
+
+  def end_date
+    @start_date + (@duration * 60) # Convert minutes to seconds
+  end
+
+  def is_past?
+    @start_date < Time.now
+  end
+
+  def is_future?
+    @start_date > Time.now
+  end
+#time.strftime("%H:%M:%S %d/%m/%Y") time format Euopeen
+  def is_soon?
+    time_difference = @start_date - Time.now
+    time_difference <= 30 * 60 # 30 minutes in seconds
+  end
+
+  def to_s
+    puts "Titre : #{@title}"
+    puts "Date de début : #{@start_date}"
+    puts "Durée : #{@duration} minutes"
+    puts "Invités : #{@attendees.join(', ')}"
+  end
+
+  def postpone_24h
+    @start_date += 24 * 60 * 60 # 24 hours in seconds
+  end
 end
+
+# Créer un nouvel événement
+event = Event.new("2023-08-19 14:00", 120, "Réunion importante", ["john@example.com", "jane@example.com"])
+
+# Afficher les informations de l'événement
+event.to_s
+
+# Vérifier si l'événement est dans moins de 30 minutes
+if event.is_soon?
+  puts "L'événement est bientôt !"
+end
+
+# Reporter l'événement au lendemain
+puts "postpone_24h : #{event.postpone_24h}"
+
+# Afficher les informations mises à jour
+event.to_s
--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,26 @@
 class User
+  attr_accessor :email, :age
+  @@user_count = 0
+  def initialize(email_save, age_save)
+    @email = email_save
+    @age = age_save
+    @@user_count += 1
+  end
 
+  def self.all
+    return @@user_count
+  end
+  
+  def to_s
+    puts "Email : #{@email}"
+    puts "Age : #{@age}"
+  end
 end
+habib = User.new("faye@gmail.com", 24)
+ibrahima = User.new("ndao@gmail.com", 25)
+habib.to_s
+puts "-----------------------------------"
+ibrahima.to_s
+puts
+p User.all
+puts "***********************************************"
